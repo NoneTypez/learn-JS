86e52b45158f6d60885657850afcceb799a9754a
NoneTypez <graffity555@gmail.com>
2025-03-30 19:42:54 +0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const lines = 5;
-
+// MAKING A CORRECT TRIANGLE 
 const foo = (lines) => {
   let res = ''
   let zeroCount = 1
-  for (let i = lines; i >= 1; i--) {
+  for (let i = lines; i >= 0; i--) {
     res+=" ".repeat(i);
     res+= '*'.repeat(zeroCount);
     res+='\n'
